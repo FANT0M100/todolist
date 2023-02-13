0de2a471351bf2a3ba8753a8e990518342e8fc01
FANT0M100 <giorgie103188@gmail.com>
2023-02-13 12:14:27 +0400

    en create task. start add list

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import ListSection from './components/listSection';
 
 function App() {
    
-  const [todoList, useTodoList] = useState([])
+  const [todoList, setTodoList] = useState([])
+  const [filterVal, setFilterVal] = useState('all')
 
   return (
     <div>
@@ -14,9 +15,17 @@ function App() {
         <div className='h-100 w-full flex items-center justify-center font-sans'>
           <div className='bg-white rounded shadow-lg p-6 m-4 w-full lg:w-1/2'>
             {/* headre */}
-            <HeaderSection/>
+            <HeaderSection
+             todoList={todoList}
+             setTodoList={setTodoList}
+             filterVal={filterVal}
+             setFilterVal={setFilterVal}
+            />
             {/* todo list */}
-            <ListSection/>
+            <ListSection
+              todoList={todoList}
+              setTodoList={setTodoList}
+            />
           </div>
         </div>        
       </div>
--- src/components/headerSection.jsx
+++ src/components/headerSection.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-function HeaderSection() {
+function HeaderSection( { todoList, setTodoList, filterVal, setFilterVal } ) {
+
+    const [task, setTask] = useState('')
+    
+    const handleAdd =() => {
+        
+        setTodoList(pre => [...pre, { task, id: uuidv4(), inCompleted:false }])
+        setTask('')
+    }
+
+    const selectedValues = [
+        {value:'all', name:'All'},
+        {value:'completed', name:'Completed'},
+        {value:'uncompleted', name:'Uncompleted'}
+    ]
     return(
         <div className="mb-8">
             <h1 className="text-3xl text-grey-darker">Gio's Todo List</h1>
             <div className="flex mt-4 gap-4">
                 <input className="rounded border appearance-none shadow w-full py-2 px-3 text-grey-darker"
                  placeholder="My task" 
+                 value={task}
+                 onChange={(e) => setTask(e.target.value)}
                 />
-                <button className="p-2 border-2 rounded text-teal-900 border-teal hover:text-white hover:bg-teal-700 duration-500">
+                <button 
+                    onClick={() => handleAdd()}
+                    className="p-2 border-2 rounded text-teal-900 border-teal hover:text-white hover:bg-teal-700 duration-500">
                     Add
                 </button>
-                <select className="shadow appearance-none border rounded py-2 px-3 text-grey-darker flex-no-grow">
-                    <option>All</option>
-                    <option>Completed</option>
-                    <option>Uncompleted</option>
+                <select
+                    onChange={(e) => setFilterVal(e.target.value)}
+                    value={filterVal} 
+                    className="shadow appearance-none border rounded py-2 px-3 text-grey-darker flex-no-grow">
+                    {/* <option value='all'>all</option>
+                    <option value='completed'>completed</option>
+                    <option value='uncompleted'>uncompleted</option> */}
+                    {selectedValues.map((item, index) => (
+                        <option value={item.value} key={`filter_values ${index}`}>{item.name}</option>
+                    ))}
                 </select>
             </div>
         </div>
